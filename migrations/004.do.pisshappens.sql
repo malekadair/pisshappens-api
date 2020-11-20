ae6c6dda1c9b3f640511b6784fddceaf70eab23b
Malek Haj-Hussein <malekadair@gmail.com>
2020-11-19 21:33:58 -0600

    Had to change Knex node module because of Regex error. updated server connection and tested seeding of db

--- migrations/004.do.pisshappens.sql
+++ migrations/004.do.pisshappens.sql
@@ -1,5 +1,28 @@
-BEGIN
+BEGIN;
 
+TRUNCATE
+	comments,
+	comics,
+	users;
 
+INSERT INTO users (user_name, full_name, password)
+VALUES
+  ('admin', 'Administrator Gary', '$2a$12$rugz4SDjwR.33.UL1fOlQ.qMmpZ5MfRQI6dXJi8w4vj526IFM5eou'),
+  ('malek', 'Malek Haj-Hussein', '$2a$12$3D/FNCVfI/RkbY.pw6HmbevZSFBBwxoHoIk/YTRVLQdIh9C0ECgYS'),
+  ('grace', 'Grace Bodur', '$2a$12$pOde1szWj5A6dGcODWCUY.KHFKqkMuGuUvRPDBm73FP9xwxxHEUlC');
+
+INSERT INTO comics (creator_id, title, description)
+VALUES
+  (1, 'HAPPY comic', 'blah blah blah :)'),
+  (1, 'UNHAPPY comic', 'blah :((((((('),
+  (3, 'mad comic', 'BLAH!');
+  (2, 'just an ordinary comic', 'blah.');
+
+INSERT INTO comments (comic_id, user_id, comment)
+VALUES
+  (1, 2, 'this is funny'),
+  (1, 2, 'this is not funny'),
+  (3, 3, 'THIS IS NOT FUNNY!!!!');
+  (2, 1, 'this is a comic');
 
 COMMIT;
--- src/comics/comics-router.js
+++ src/comics/comics-router.js
@@ -5,23 +5,24 @@ const jsonBodyParser = express.json();
 const comicsRouter = express.Router();
 
 const serializeComic = comic => ({
-	guess_id: guess.guess_id,
-	user_id: guess.user_id,
-	week_id: guess.week_id,
-	guess_1: xss(guess.guess_1),
-	guess_2: xss(guess.guess_2),
-	guess_3: xss(guess.guess_3),
-	guess_4: xss(guess.guess_4),
-	guess_5: xss(guess.guess_5),
-	power_ball: xss(guess.power_ball),
-	guess_created_date: guess.guess_created_date,
-	message: xss(guess.message),
-	has_won: guess.has_won,
-	user_name: guess.user_name,
-	password: guess.password,
-	user_created_date: guess.user_created_date,
-	date_modified: guess.date_modified,
-	week_start_date: guess.week_start_date
+
+	// guess_id: guess.guess_id,
+	// user_id: guess.user_id,
+	// week_id: guess.week_id,
+	// guess_1: xss(guess.guess_1),
+	// guess_2: xss(guess.guess_2),
+	// guess_3: xss(guess.guess_3),
+	// guess_4: xss(guess.guess_4),
+	// guess_5: xss(guess.guess_5),
+	// power_ball: xss(guess.power_ball),
+	// guess_created_date: guess.guess_created_date,
+	// message: xss(guess.message),
+	// has_won: guess.has_won,
+	// user_name: guess.user_name,
+	// password: guess.password,
+	// user_created_date: guess.user_created_date,
+	// date_modified: guess.date_modified,
+	// week_start_date: guess.week_start_date
 });
 
 comicsRouter
@@ -29,7 +30,7 @@ comicsRouter
 	.get((req, res, next) => {
 		ComicsService.getAllComics(req.app.get('db'))
 			.then(comics => {
-				res.json(comics.map(serializeComic))
+				res.json(comics)
 			})
 			.catch(next)
 
