f36531b7f7f0cbdae5779fc6b58c37d16378107f
Malek Haj-Hussein <malekadair@gmail.com>
2020-11-23 17:41:17 -0600

    Added postgrator config file and starting to set up migrations to run properly.

--- src/comics/comics-router.js
+++ src/comics/comics-router.js
@@ -35,21 +35,72 @@ comicsRouter
 					.json(comics)
 			})
 			.catch(next)
-
+		// 		.post(jsonBodyParser, (req, res, next) => {
+		// 			const { user_id, week_id, guess_1, guess_2, guess_3, guess_4, guess_5, power_ball, message } = req.body
+		// 	const newGuess = {
+		// 		user_id,
+		// 		week_id,
+		// 		guess_1,
+		// 		guess_2,
+		// 		guess_3,
+		// 		guess_4,
+		// 		guess_5,
+		// 		power_ball,
+		// 		message
+		// 	}
+		// 			for (const [key, value] of Object.entries(newComic))
+		// 				if (value == null)
+		// 					return res.status(400).json({
+		// 						error: `Missing '${key}' in request body`
+		// 					});
+		// 			GuessesService.insertGuess(req.app.get("db"), newGuess)
+		// 				.then(guess => {
+		// 					res
+		// 						.status(201)
+		// 						.json(guess);
+		// 				})
+		// 				.catch(next);
+		// 		})
 
 	})
 
 
 comicsRouter
 	.route('/:comic_id')
+	// .all(checkComicExists)
+	// .get((req, res) => {
+	// 	res.json(res.comic);
+	// })
 	.get((req, res, next) => {
 		ComicsService.getComicById(
 			req.app.get('db'),
 			req.params.comic_id
 		)
 			.then(comic => {
-				res.json(comic);
+				res
+					.status(200)
+					.json(comic);
 			})
 			.catch(next)
 	})
+// async function checkComicExists(req, res, next) {
+// 	try {
+// 		const comic = await ComicsService.getComicById(
+// 			req.app.get("db"),
+// 			req.params.comic_id
+// 		);
+
+// 		if (!comic)
+// 			return res
+// 				.status(404)
+// 				.json({
+// 					error: `Comic doesn't exist`
+// 				});
+
+// 		res.comic = comic;
+// 		next();
+// 	} catch (error) {
+// 		next(error);
+// 	}
+// }
 module.exports = comicsRouter
--- src/comics/comics-service.js
+++ src/comics/comics-service.js
@@ -4,23 +4,18 @@ const ComicsService = {
 			.from('comics')
 	},
 
-	getComicByWeekId(db, week_id) {
-		return ComicsService.getAllComics(db)
-			.where('comics.week_id', week_id)
-	},
-
 	getComicById(db, comic_id) {
 		return ComicsService.getAllComics(db)
-		// .where('comics.id', comic_id)
-		// .first();
+			.where('comics.id', comic_id)
+			.first();
 	},
 	insertComic(db, newComic) {
 		return db
 			.insert(newComic)
 			.into("comics")
 			.returning("*")
-			.then(([comic]) => comic)
-			.then(comic => ComicsService.getById(db, comic.week_id));
+		// .then(([comic]) => comic)
+		// .then(comic => ComicsService.getComicById(db, comic.id));
 	},
 }
 
